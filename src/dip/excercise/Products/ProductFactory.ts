b5ec32c584b6446f36e13dbf5a5f3f539f9cc3bf
Frankeo <ingfrancisco.moreno1@gmail.com>
2022-09-15 23:37:44 +0000

    Fix factory product implementation

--- src/dip/excercise/Products/ProductFactory.ts
+++ src/dip/excercise/Products/ProductFactory.ts
@@ -1,11 +1,12 @@
 import { IProductInfo } from '../Core/IProductInfo';
 import { PriceProduct } from './PriceProduct';
+import { TitleProduct } from './TitleProduct';
 
 export class ProductFactory {
     static getInstance(typeOfProduct: string, id: number, price: number, title: string): IProductInfo {
         switch (typeOfProduct) {
             case 'Title':
-              return new PriceProduct(id, title, price);
+              return new TitleProduct(id, title, price);
             case 'Price':
               return new PriceProduct(id, title, price);
             default:
