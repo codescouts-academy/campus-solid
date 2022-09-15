cec0e7442276780e1f7d0c1d277ab73d13bc3d74
Frankeo <ingfrancisco.moreno1@gmail.com>
2022-09-15 18:20:46 +0000

    Remove srp, ocp and lsp code

--- src/lsp/excercise/IProductInfo.ts
+++
@@ -1,5 +0,0 @@
-import { Market } from "./Market";
-
-export interface IProductInfo {
-  getInformation(market: Market): string;
-}
--- src/ocp/excercise/ITax.ts
+++
@@ -1,5 +0,0 @@
-import { Product } from "./Product";
-
-export interface ITax {
-  getTax(product: Product): number;
-}
